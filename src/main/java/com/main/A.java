2517a7799ec8efdb70561c6b3a65e02ca90c1ad1
vaishalivasu <vaishali.v1920@gmail.com>
2025-05-29 17:17:13 +0530

    commit1

---
+++ src/main/java/com/main/A.java
@@ -0,0 +1,10 @@
+package com.main;
+
+import org.testng.annotations.Test;
+
+public class A {
+@Test
+public void atest() {
+	System.out.println("hi");
+}
+}
---
+++ src/test/java/com/test/B.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class B {
+@Test
+public void btest() {
+	System.out.println("hello");
+}
+}
